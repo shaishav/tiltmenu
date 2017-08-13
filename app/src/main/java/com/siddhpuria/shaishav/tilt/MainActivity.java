be76282a9674e9337fcbc1517b1fd61f45e25972
Shaishav Siddhpuria <spsiddhp@uwaterloo.ca>
2017-08-13 16:37:52 -0400

    Add a separate full-screen experiment activity to display the tilt menu
    The MainActivity intent will provide the required configuration details as a string before launching this activity. The ExperimentActivity will then draw itself based on the message received (e.g. screen size, shape, etc.) and start the experiment session.

--- app/src/main/java/com/siddhpuria/shaishav/tilt/MainActivity.java
+++ app/src/main/java/com/siddhpuria/shaishav/tilt/MainActivity.java
@@ -1,13 +1,40 @@
 package com.siddhpuria.shaishav.tilt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXPERIMENT_CONFIG = "com.shaishav.siddhpuria.mainactivity.experimentconfig";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button launchExperimentButton = (Button) findViewById(R.id.start_experiment_button);
+        launchExperimentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchExperimentActivity();
+            }
+        });
+
+
     }
+
+    public void launchExperimentActivity() {
+        Intent intent = new Intent(this, ExperimentActivity.class);
+        String configString = getExperimentConfig();
+        intent.putExtra(EXPERIMENT_CONFIG, configString);
+        startActivity(intent);
+    }
+
+    public String getExperimentConfig() {
+        return "";
+    }
+
 }
