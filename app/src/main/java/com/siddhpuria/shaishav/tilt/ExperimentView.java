46a2fdc6f43090d6624aab1cac94dc0608afb4f3
Shaishav Siddhpuria <spsiddhp@uwaterloo.ca>
2017-08-13 17:21:04 -0400

    Add a test sphere drawable

--- app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
+++ app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
@@ -3,7 +3,12 @@ package com.siddhpuria.shaishav.tilt;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 /**
@@ -13,26 +18,49 @@ import android.view.View;
 
 public class ExperimentView extends View {
 
-    Paint paint = null;
+    Pointer pointer;
 
     public ExperimentView(Context context) {
         super(context);
-        paint = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
-        int x = getWidth();
-        int y = getHeight();
-        int radius = 100;
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.BLACK);
-        canvas.drawPaint(paint);
-        paint.setColor(Color.RED);
-        canvas.drawCircle(x/2, y/2, radius, paint);
-        System.out.println("DRAWING");
+        pointer = new Pointer();
+        pointer.draw(canvas);
 
+    }
+
+    public class Pointer extends Drawable {
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+            int x = canvas.getWidth();
+            int y = canvas.getHeight();
+            int radius = 100;
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.BLACK);
+            canvas.drawPaint(paint);
+            paint.setColor(Color.RED);
+            canvas.drawCircle(x/2, y/2, radius, paint);
+        }
+
+        @Override
+        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setColorFilter(@Nullable ColorFilter colorFilter) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int getOpacity() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
