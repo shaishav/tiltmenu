b5eaa2f7ef10ec6385c69f83cf5675a92aa82812
Shaishav Siddhpuria <spsiddhp@uwaterloo.ca>
2017-08-15 18:02:39 -0400

    Finish layout drawing for 2-zone and 4-zone menu
    The two menu are Drawable subclasses of the abstract ZoneMenu class which is responsible for drawing the menu on screen and provide updates for when a selection is made. The protocol for menu selection is yet to be decided but will probably involve communicating an integer index to the parent ExperimentActivity class which will then process and update the next screen accordingly.

--- app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
+++ app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
@@ -18,18 +19,30 @@ import android.view.View;
 
 public class ExperimentView extends View {
 
-    Pointer pointer;
+    private Pointer pointer;
+    private MenuZone menuZone;
 
     public ExperimentView(Context context) {
         super(context);
         pointer = new Pointer();
+        menuZone = new MenuZoneFour();
+        String menu[] = {"A", "B", "C", "D"};
+        menuZone.setOptions(menu);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+
+        menuZone.draw(canvas);
         pointer.draw(canvas);
+
 
     }
 
@@ -44,8 +57,9 @@ public class ExperimentView extends View {
      */
     public class Pointer extends Drawable {
 
-        float x, y;
-        Canvas canvas = null;
+        private float x, y;
+        private float centerX, centerY;
+        private Canvas canvas = null;
 
         @Override
         public void draw(@NonNull Canvas canvas) {
@@ -53,13 +67,12 @@ public class ExperimentView extends View {
                 this.canvas = canvas;
                 x = canvas.getWidth()/2;
                 y = canvas.getHeight()/2;
+                centerX = x;
+                centerY = y;
             }
 
             int radius = 50;
             Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.BLACK);
-            canvas.drawPaint(paint);
             paint.setColor(Color.RED);
             canvas.drawCircle(x, y, radius, paint);
         }
@@ -67,24 +80,200 @@ public class ExperimentView extends View {
         public void drawAtOffset(float xOffset, float yOffset) {
             if (canvas == null) return;
 
-            x = Math.min(canvas.getWidth(), Math.max(0, canvas.getWidth()/2 + xOffset));
-            y = Math.min(canvas.getHeight(), Math.max(0, canvas.getHeight()/2 + yOffset));
-//            System.out.println("x "+x+" y "+y);
+            x = this.centerX + xOffset;
+            y = this.centerY + yOffset;
+
+            // using java's reflection for bounds checking
+            if (menuZone.getClass() == MenuZoneTwo.class) {
+
+                double magnitude = Math.sqrt(xOffset*xOffset + yOffset*yOffset);
+                xOffset /= magnitude;
+                yOffset /= magnitude;
+
+                float boundRadius = ((MenuZoneTwo) menuZone).getBoundRadius();
+
+                if (magnitude > boundRadius) {
+                    xOffset *= boundRadius;
+                    yOffset *= boundRadius;
+
+                    x = this.centerX + xOffset;
+                    y = this.centerY + yOffset;
+
+                    if (y < this.centerY) {
+                        String[] newMenu = {"AB", "CD"};
+                        menuZone.setOptions(newMenu);
+                    } else {
+                        String[] newMenu = {"CD", "AB"};
+                        menuZone.setOptions(newMenu);
+                    }
+                }
+            } else if (menuZone.getClass() == MenuZoneFour.class) {
+
+                double magnitude = Math.sqrt(xOffset*xOffset + yOffset*yOffset);
+                Rect bounds = ((MenuZoneFour) menuZone).getSquareBounds();
+
+                xOffset = Math.min(bounds.right, Math.max(bounds.left, this.centerX+xOffset));
+                yOffset = Math.min(bounds.bottom, Math.max(bounds.top, this.centerY+yOffset));
+
+                x = xOffset;
+                y = yOffset;
+
+            }
         }
 
         @Override
-        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
-            throw new UnsupportedOperationException();
-        }
+        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) { throw new UnsupportedOperationException(); }
 
         @Override
-        public void setColorFilter(@Nullable ColorFilter colorFilter) {
-            throw new UnsupportedOperationException();
-        }
+        public void setColorFilter(@Nullable ColorFilter colorFilter) { throw new UnsupportedOperationException(); }
 
         @Override
         public int getOpacity() {
             throw new UnsupportedOperationException();
         }
+    }
+
+    public abstract class MenuZone extends Drawable {
+
+        protected int lightBlueColor = Color.argb(255, 218, 237, 255);
+        protected int darkBlueColor = Color.argb(255, 182, 221, 255);
+        protected float centerX;
+        protected float centerY;
+        protected Canvas canvas;
+        protected  int numVisibleItems = 0;
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+
+            if (this.canvas == null) {
+                this.canvas = canvas;
+                this.centerX = canvas.getWidth()/2;
+                this.centerY = canvas.getHeight()/2;
+            }
+
+        }
+
+        public abstract void setOptions(String[] options);
+
+    }
+
+    public class MenuZoneTwo extends MenuZone {
+
+        private float innerRadius = 250.0f;
+        private float outerRadius = 350.0f;
+        private String[] menuValues = new String[2];
+
+        public MenuZoneTwo() {
+            super();
+            numVisibleItems = 2;
+        }
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+            super.draw(canvas);
+
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+            paint.setColor(this.darkBlueColor);
+            canvas.drawCircle(this.centerX, this.centerY, outerRadius, paint);
+
+            paint.setColor(this.lightBlueColor);
+            canvas.drawCircle(this.centerX, this.centerY, innerRadius, paint);
+
+            // draw string for menus
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(48.0f);
+            canvas.drawText(menuValues[0], this.centerX - getBoundRadius()/4, this.centerY - getBoundRadius()/2, paint);
+            canvas.drawText(menuValues[1], this.centerX - getBoundRadius()/4, this.centerY + getBoundRadius()/2, paint);
+
+            // draw middle line
+            paint.setStrokeWidth(4.0f);
+            canvas.drawLine(this.centerX - outerRadius, this.centerY, this.centerX + outerRadius, this.centerY, paint);
+        }
+
+        public float getBoundRadius() {
+            return (innerRadius+outerRadius)/2.0f;
+        }
+
+        @Override
+        public void setOptions(String[] options) {
+            menuValues = options;
+            invalidate();
+        }
+
+        public int getNumVisibleItems() { return numVisibleItems; }
+
+        @Override
+        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public void setColorFilter(@Nullable ColorFilter colorFilter) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public int getOpacity() { throw new UnsupportedOperationException(); }
+
+    }
+
+    public class MenuZoneFour extends MenuZone {
+
+        private float innerRadius = 250.0f;
+        private float outerRadius = 350.0f;
+        private String[] menuValues = new String[4];
+
+        public MenuZoneFour() {
+            super();
+            numVisibleItems = 4;
+        }
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+            super.draw(canvas);
+
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+            paint.setColor(this.darkBlueColor);
+            canvas.drawRect(this.centerX-outerRadius, this.centerY-outerRadius, this.centerX+outerRadius, this.centerY+outerRadius, paint);
+
+            paint.setColor(this.lightBlueColor);
+            canvas.drawRect(this.centerX-innerRadius, this.centerY-innerRadius, this.centerX+innerRadius, this.centerY+innerRadius, paint);
+
+            // draw string for menus
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(48.0f);
+            canvas.drawText(menuValues[0], this.centerX, this.centerY - outerRadius/1.5f, paint);
+            canvas.drawText(menuValues[1], this.centerX + outerRadius/1.5f, this.centerY, paint);
+            canvas.drawText(menuValues[2], this.centerX, this.centerY + outerRadius/1.5f, paint);
+            canvas.drawText(menuValues[3], this.centerX - outerRadius/1.5f, this.centerY, paint);
+
+            // draw middle line
+            paint.setStrokeWidth(4.0f);
+            canvas.drawLine(this.centerX - outerRadius, this.centerY - outerRadius, this.centerX + outerRadius, this.centerY + outerRadius, paint);
+            canvas.drawLine(this.centerX + outerRadius, this.centerY - outerRadius, this.centerX - outerRadius, this.centerY + outerRadius, paint);
+        }
+
+        public Rect getSquareBounds() {
+            float midPoint = (innerRadius+outerRadius)/2.0f;
+            return new Rect((int) (this.centerX-midPoint), (int) (this.centerY-midPoint), (int) (this.centerX+midPoint), (int) (this.centerY+midPoint));
+        }
+
+        @Override
+        public void setOptions(String[] options) {
+            menuValues = options;
+            invalidate();
+        }
+
+        public int getNumVisibleItems() { return numVisibleItems; }
+
+        @Override
+        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public void setColorFilter(@Nullable ColorFilter colorFilter) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public int getOpacity() { throw new UnsupportedOperationException(); }
+
     }
 }
