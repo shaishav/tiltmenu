9cc580816c4c0aafc54404c3e7e72500486e5305
Shaishav Siddhpuria <spsiddhp@uwaterloo.ca>
2017-08-15 15:35:28 -0400

    Implement offset-based pointer movement
    The pointer will move in the direction of the tilt with an added gain value.

--- app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
+++ app/src/main/java/com/siddhpuria/shaishav/tilt/ExperimentView.java
@@ -22,30 +22,54 @@ public class ExperimentView extends View {
 
     public ExperimentView(Context context) {
         super(context);
+        pointer = new Pointer();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
-        pointer = new Pointer();
         pointer.draw(canvas);
 
     }
 
+    public void updatePointerPosition(float xOffset, float yOffset) {
+        pointer.drawAtOffset(xOffset, yOffset);
+        this.invalidate();
+    }
+
+    /**
+     * Pointer object to navigate for menu selection.
+     * This will be drawn at relative x,y offsets to center of the interaction being (0,0)
+     */
     public class Pointer extends Drawable {
+
+        float x, y;
+        Canvas canvas = null;
 
         @Override
         public void draw(@NonNull Canvas canvas) {
-            int x = canvas.getWidth();
-            int y = canvas.getHeight();
-            int radius = 100;
+            if (this.canvas == null) {
+                this.canvas = canvas;
+                x = canvas.getWidth()/2;
+                y = canvas.getHeight()/2;
+            }
+
+            int radius = 50;
             Paint paint = new Paint();
             paint.setStyle(Paint.Style.FILL);
             paint.setColor(Color.BLACK);
             canvas.drawPaint(paint);
             paint.setColor(Color.RED);
-            canvas.drawCircle(x/2, y/2, radius, paint);
+            canvas.drawCircle(x, y, radius, paint);
+        }
+
+        public void drawAtOffset(float xOffset, float yOffset) {
+            if (canvas == null) return;
+
+            x = Math.min(canvas.getWidth(), Math.max(0, canvas.getWidth()/2 + xOffset));
+            y = Math.min(canvas.getHeight(), Math.max(0, canvas.getHeight()/2 + yOffset));
+//            System.out.println("x "+x+" y "+y);
         }
 
         @Override
